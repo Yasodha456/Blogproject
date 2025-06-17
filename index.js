b93155d8746968a76af5f0b5e6d30ad33565930c
Yasodha456 <yasodhamercy27@gmail.com>
2025-06-17 18:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const express = require('express');
-// const connectDB = require('./db.js');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const app = require('./config');
-
-// dotenv.config();
-// const app = express();
-// connectDB();
-
-// app.use(cors());
-// app.use(express.json());
-
-// const blogRoutes = require('../Routes/copy');
-// const authRoutes = require('../Routes/authCopy');
-
-// app.use('/api/blogs', blogRoutes);
-// app.use('/api/auth', authRoutes);
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -38,14 +12,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Import routes
 const blogRoutes = require('./Routes/copy');
 const authRoutes = require('./Routes/authCopy');
 
 app.use('/api/blogs', blogRoutes);
 app.use('/api/auth', authRoutes);
 
-// Start server
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
